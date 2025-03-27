8022c3f0f9d82b5290c90ceeef856396619d67fe
Seba <s.radawiec@outlook.com>
2025-03-27 16:38:41 +0100

    testy sparametryzowane

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,9 +1,20 @@
+import pytest
+
 def add(a,b):
     return a + b
 
 def test_add():
     assert add(2,3) == 5
 
-#def test_add_not_good():
-#    assert add(2,3) == 55
-    
+# #def test_add_not_good():
+#     assert add(2,3) == 55
+
+
+@pytest.mark.parametrize("a,b,wynik",[
+    (2,3,5),
+    (1,1,2),
+    (0,0,0)
+])
+
+def test_param_add(a,b,wynik):
+    assert add(a,b) == wynik
